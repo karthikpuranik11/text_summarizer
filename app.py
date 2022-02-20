8df6872384f9bf7067bc9f3568ae5ac09aa43afd
Karthik Puranik <56488442+karthikpuranik11@users.noreply.github.com>
2022-02-20 14:58:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,78 +2,74 @@ import numpy as np
 import pandas as pd
 import streamlit as st 
 import joblib
+import os
+from moviepy.editor import *
+import speech_recognition as sr
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+import transformers
+from transformers import pipeline
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+from nltk.tokenize import sent_tokenize
+import re
 
 from PIL import Image
-
-regressor=joblib.load("regressor.pkl")
-x_train=joblib.load("dataset.pkl")
 
 
 def welcome():
     return "Welcome All"
 
-@st.cache
-def predict_note_authentication(rooms,department,ward,doctor,staff,idd,age,gender,typee,severity,conditions,visitors,insurance,deposit):
-    l=[rooms,department,ward,doctor,staff,idd,age,gender,typee,severity,conditions,visitors,insurance,deposit]
-    cols=['Available Extra Rooms in Hospital', 'Department', 'Ward_Facility_Code', 'doctor_name', 'staff_available', 'patientid', 'Age', 'gender', 'Type of Admission', 'Severity of Illness', 'health_conditions',
-       'Visitors with Patient', 'Insurance', 'Admission_Deposit']
-    data2=pd.DataFrame([l], columns=cols)
-    data2 = pd.get_dummies( data2, columns=data2.select_dtypes(include=["object", "category"]).columns.tolist(), drop_first=True,)
-    data2 = data2.reindex(columns = x_train.columns, fill_value=0)
-    prediction=regressor.predict(data2)
-    print(prediction)
-    return int(round(prediction[0], 0))
+@st.cache(allow_output_mutation=True)
+def load_summarizer():
+    model = pipeline("summarization", device=0)
+    return model
 
 
-
-def main():
-    #st.title("Length of stay predictor")
-    html_temp = """
-    <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Streamlit Length of stay predictor </h2>
-    </div>
-    """
-    st.markdown(html_temp,unsafe_allow_html=True)
-
-    col1, col2 = st.columns(2)
-    rooms = col1.number_input("Available Extra Rooms in Hospital", min_value=0, max_value=25)
-    department = col2.selectbox("Department",("gynecology", "radiotherapy", "anesthesia","TB & Chest disease", "surgery" ))
-    ward = col1.selectbox("Ward Facility Code",("A","B","C","D", "E", "F"))
-    doctor = col2.selectbox("Doctor Name",("Dr Sarah", "Dr Olivia", "Dr Sophia", "Dr Nathan" ,"Dr Sam", "Dr John", "Dr Mark", "Dr Isaac", "Dr Simon"))
-    staff = col1.number_input("Staff available", min_value=0, max_value=15)
-    idd = col2.number_input("Patient ID", min_value=30000, max_value=99999)
-    age = col1.selectbox("Age",("0-10","11-20","21-30","31-40","41-50","51-60","61-70", "71-80", "81-90","91-100"))
-    gender=col2.selectbox("Gender", ("Male","Female", "Other"))
-    typee=col1.selectbox("Type of admission", ("Trauma", "Emergency", "Urgent"))
-    severity=col2.selectbox("Severty of illness", ("Minor", "Moderate", "Extreme"))
-    conditions=col1.selectbox("Previous health conditions", ("High Blood Pressure","Diabetes","Asthama","Heart disease", "None"," Other"))
-    visitors=col2.number_input("Number of visitors", min_value=0, max_value=50)
-    insurance=col1.selectbox("Health insurance?", ("Yes", "No"))
-    deposit=col2.number_input("Admission deposit", min_value=0, max_value=100000)
-    result=""
-    if st.button("Predict"):
-        result=predict_note_authentication(rooms,department,ward,doctor,staff,idd,age,gender,typee,severity,conditions,visitors,insurance,deposit)
-    st.success('The output is {}'.format(result)+ " days")
-    if st.button("About"):
-        st.subheader("Data dictionary:")
-        st.text("Available Extra Rooms in Hospital: The number of rooms available during admission.")
-        st.text("Department: The department which will be treating the patient.")
-        st.text("Ward Facility Code: The code of the ward facility in which the patient will be admitted.")
-        st.text("Doctor Name: The doctor who will be treating the patient.")
-        st.text("Staff Available: The number of staff who are not occupied at the moment in the ward.")
-        st.text("Patient ID: ID of the patient admitted")
-        st.text("Age: Range of age of the patient")
-        st.text("Gender: Gender of the patient.")
-        st.text("Type of Admission: Trauma, emergency or urgent.")
-        st.text("Severity of Illness: Extreme, moderate or minor.")
-        st.text("Previous health conditions: Any previous health conditions suffered by the patient.")
-        st.text("Number of visitors: The number of patients who accompany the patient.")
-        st.text("Health insurance?: Does the patient have health insurance or not?")
-        st.text("Admission Deposit: The deposit paid by the patient during admission.")
-        st.text("Stay (in days): The number of days that the patient has stayed in the hospital.")
-        
-
-if __name__=='__main__':
-    main()
+def generate_chunks(inp_str):
+    max_chunk = 500
+    inp_str = inp_str.replace('.', '.<eos>')
+    inp_str = inp_str.replace('?', '?<eos>')
+    inp_str = inp_str.replace('!', '!<eos>')
     
+    sentences = inp_str.split('<eos>')
+    current_chunk = 0 
+    chunks = []
+    for sentence in sentences:
+        if len(chunks) == current_chunk + 1: 
+            if len(chunks[current_chunk]) + len(sentence.split(' ')) <= max_chunk:
+                chunks[current_chunk].extend(sentence.split(' '))
+            else:
+                current_chunk += 1
+                chunks.append(sentence.split(' '))
+        else:
+            chunks.append(sentence.split(' '))
+
+    for chunk_id in range(len(chunks)):
+        chunks[chunk_id] = ' '.join(chunks[chunk_id])
+    return chunks
+
+
+summarizer = load_summarizer()
+st.title("Summarize Text")
+sentence = st.text_area('Please paste your article :', height=30)
+button = st.button("Summarize")
+
+max = st.sidebar.slider('Select max', 50, 500, step=10, value=150)
+min = st.sidebar.slider('Select min', 10, 450, step=10, value=50)
+do_sample = st.sidebar.checkbox("Do sample", value=False)
+with st.spinner("Generating Summary.."):
+    if button and sentence:
+        chunks = generate_chunks(sentence)
+        res = summarizer(chunks,
+                         max_length=max, 
+                         min_length=min, 
+                         do_sample=do_sample)
+        text = ' '.join([summ['summary_text'] for summ in res])
+        # st.write(result[0]['summary_text'])
+        st.write(text)
     
