2d57081b949a0322d2c576f3bff55ce89c60dc30
Karthik Puranik <56488442+karthikpuranik11@users.noreply.github.com>
2022-02-20 16:56:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,10 +23,6 @@ from PIL import Image
 def welcome():
     return "Welcome All"
   
-def mp4_to_wav():
-  video = VideoFileClip(os.path.join("video.mp4"))
-  video.audio.write_audiofile(wav, format='.wav')
-  return wav  
     
 st.title("Summarize Text")
 video = st.file_uploader("Choose a file", type=['mp4'])
@@ -36,5 +32,5 @@ max = st.sidebar.slider('Select max', 50, 500, step=10, value=150)
 min = st.sidebar.slider('Select min', 10, 450, step=10, value=50)
 with st.spinner("Generating Summary.."):
     if button and video:
-        st.write(mp4_to_wav())
+        st.video(video, format="video/mp4", start_time=0)
     
