27c2d94ffde1e8adf81d44e4ef2beaa5b4b398db
Karthik Puranik <56488442+karthikpuranik11@users.noreply.github.com>
2022-02-27 21:47:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,8 @@ max = st.sidebar.slider('Select max', 50, 500, step=10, value=150)
 min = st.sidebar.slider('Select min', 10, 450, step=10, value=50)
 with st.spinner("Generating Summary.."):
     if button and video:
-        #v = VideoFileClip(video)
+        video1=open(video, 'rb').read()
+        v = VideoFileClip(video1)
         #v.audio.write_audiofile("movie.wav")
         st.video(video, format="video/mp4", start_time=0)
         #st.audio("movie.wav")
