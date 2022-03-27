bc48d3859032afb197e522df1be8fe2b8286e184
Karthik Puranik <56488442+karthikpuranik11@users.noreply.github.com>
2022-03-27 23:31:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,21 +48,15 @@ def get_large_audio_transcription(path):
     
 st.title("Summarize Text")
 video = st.file_uploader("Choose a file", type=['mp4'])
-#bytes=video.read()
-#v = VideoFileClip(bytes)
-#st.write("completed")
-#video.export("video1.mp4", format="mp4")
+video_filename = os.path.join(f"video1.mp4")
+video.export(video_filename, format="mp4")
 button = st.button("Summarize")
 
 max = st.sidebar.slider('Select max', 50, 500, step=10, value=150)
 min = st.sidebar.slider('Select min', 10, 450, step=10, value=50)
 with st.spinner("Generating Summary.."):
     if button and video:
-        #st.write("step1")
-        #video1=open(bytes_data, 'rb').read()
-        #st.write("step2")
-        v = VideoFileClip("video.mp4")
-        #st.write("step3")
+        v = VideoFileClip("video1.mp4")
         v.audio.write_audiofile("movie.wav")
         #st.video(video, format="video/mp4", start_time=0)
         st.audio("movie.wav")
